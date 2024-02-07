e8137c298c530c44f518e2a72d1bc3f435eedf28
João Silva <joaosilvavic2017@gmail.com>
2024-02-06 22:50:58 -0300

    uptade_diferencial

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-let currentContent = 1;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-showContent(currentContent);
-
-function showContent(n) {
-  const contents = document.getElementsByClassName("slider-content");
-  const dots = document.getElementsByClassName("dot");
-
-  for (let i = 0; i < contents.length; i++) {
-    contents[i].style.display = "none";
-    dots[i].classList.remove("active");
-  }
-
-  contents[n - 1].style.display = "block";
-  dots[n - 1].classList.add("active");
-  currentContent = n;
+// Função para avançar/retroceder slides
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Função para escolher um slide específico
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Função para avançar os slides automaticamente
+setInterval(() => {
+  plusSlides(1);
+}, 3000); // Altere o intervalo de avanço dos slides conforme necessário (em milissegundos)
