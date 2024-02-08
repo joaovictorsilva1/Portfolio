ff5df1c84d064f6ec98559aa1a76122085f6d636
João Silva <joaosilvavic2017@gmail.com>
2024-02-08 20:37:26 -0300

    update_certficados

--- index.js
+++ index.js
@@ -31,3 +31,37 @@ function showSlides(n) {
 setInterval(() => {
   plusSlides(1);
 }, 3000); // Altere o intervalo de avanço dos slides conforme necessário (em milissegundos)
+
+
+
+  let slideIndexCertificado = 1;
+  showSlidesCertificados(slideIndexCertificado);
+
+  function plusSlidesCertificado(n) {
+    showSlidesCertificados(slideIndexCertificado += n);
+  }
+
+  function currentSlideCertificado(n) {
+    showSlidesCertificados(slideIndexCertificado = n);
+  }
+
+  function showSlidesCertificados(n) {
+    let i;
+    let slidesCertificados = document.getElementsByClassName("slide_certificado");
+    let dotsCertificados = document.getElementsByClassName("dot_certificado");
+    if (n > slidesCertificados.length) { slideIndexCertificado = 1 }
+    if (n < 1) { slideIndexCertificado = slidesCertificados.length }
+    for (i = 0; i < slidesCertificados.length; i++) {
+      slidesCertificados[i].style.display = "none";
+    }
+    for (i = 0; i < dotsCertificados.length; i++) {
+      dotsCertificados[i].className = dotsCertificados[i].className.replace(" active", "");
+    }
+    slidesCertificados[slideIndexCertificado - 1].style.display = "block";
+    dotsCertificados[slideIndexCertificado - 1].className += " active";
+  }
+
+  setInterval(() => {
+    plusSlidesCertificado(1);
+  }, 3000);
+
