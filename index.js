0f655e817402c00e5d810a1c790132292ac76c1e
João Silva <joaosilvavic2017@gmail.com>
2024-03-01 20:50:54 -0300

    hshss

--- index.js
+++ index.js
@@ -171,3 +171,12 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+}
+
